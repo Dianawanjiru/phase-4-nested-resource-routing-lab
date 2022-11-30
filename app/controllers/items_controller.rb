e2c0945a2f12352f585882e2022fb783650ccb2a
Diana <diana.wanjiru@student.moringaschool.com>
2022-11-30 05:34:28 +0300

    complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,32 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    items = Item.find(params[:id])
+    render json: items, include: :user
+  end
+
+  def create
+    items = Item.create(item_params)
+    render json: items, status: :created
+  end
+
+  private
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found_response
+    render json: { error: "not found" }, status: :not_found
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
-
+  
   def show
+    
     user = User.find_by(id: params[:id])
-    render json: user, include: :items
+    if user
+      render json: user, include: :items
+      else 
+      render json: {error: "User not found"}, status: :not_found
+    end
   end
+
+  
 
 end
